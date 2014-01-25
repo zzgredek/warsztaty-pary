3139194b0217d71b34910836d23d3cc35df40b9b
Abby Tale <abby@aspire>
2014-01-25 11:55:56 +0100

    Funkcja generujaca

---
+++ gen.h
@@ -0,0 +1,21 @@
+#ifndef GEN_H
+#define GEN_H
+
+#include <ctsdlib>
+#include <ctime>
+
+
+int generuj(int t[], int n)
+{
+
+    using namespace std;
+    for(int i=0; i<n; i++)
+    {
+        t[i]=rand()%100;
+        cout << t[i] << endl;
+    }
+
+}
+
+
+#endif // GEN_H
